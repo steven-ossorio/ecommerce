04d5261ca346e9332f21f2d6c6c02d559ed531b2
Steven Ossorio <steven.ossorio@resolutecg.com>
2023-08-16 08:52:42 -0500

    Basic settings page completed. Allow creation/deletion of store including a patch update to store name

--- ecommerce-admin/src/components/store-switcher.tsx
+++ ecommerce-admin/src/components/store-switcher.tsx
@@ -69,7 +69,7 @@ export default function StoreSwitcher({
           className={cn(className, "w-[200px] justify-between")}
         >
           <StoreIcon className="mr-2 h-4 w-4" />
-          Current Store
+          {currentStore?.label || "Select a store"}
           <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
