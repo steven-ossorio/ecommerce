81e2edca2234fba53e28fa2ec154f3ad263af96e
Steven Ossorio <usa.s.ossorio@gmail.com>
2023-08-16 09:23:06 -0500

    Adding changes again with right credentials

---
+++ ecommerce-admin/src/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,134 @@
+"use client";
+import * as z from "zod";
+import { FC, useState } from "react";
+import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+import { set, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
+
+import Heading from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { AlertModal } from "@/components/modals/alert-modal";
+import { ApiAlert } from "@/components/ui/api-alert";
+import { useOrigin } from "@/hooks/use-origin";
+
+interface SettingsFormProps {
+  initialData: Store;
+}
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+type SettingFormValues = z.infer<typeof formSchema>;
+
+export const SettingsForm: FC<SettingsFormProps> = ({ initialData }) => {
+  const params = useParams();
+  const router = useRouter();
+  const origin = useOrigin();
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<SettingFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const onSubmit = async (data: SettingFormValues) => {
+    try {
+      setLoading(true);
+      await fetch(`/api/stores/${params.storeId}`, {
+        method: "PATCH",
+        body: JSON.stringify(data),
+      });
+      router.refresh();
+      toast.success("Store updated.");
+    } catch (error) {
+      toast.error("Something went wrong.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await fetch(`/api/stores/${params.storeId}`, {
+        method: "DELETE",
+      });
+      router.refresh();
+      router.push("/");
+      toast.success("Store deleted.");
+    } catch (error) {
+      toast.error("Make sure you removed all products and categories first.");
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Manage store preferences" />
+        <Button variant="destructive" size="icon" onClick={() => setOpen(true)}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Store name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" type="submit">
+            Save changes
+          </Button>
+        </form>
+      </Form>
+      <Separator />
+      <ApiAlert
+        title="NEXT_PUBLIC_API_URL"
+        description={`${origin}/api/${params.storeId}`}
+        variant="public"
+      />
+    </>
+  );
+};
