36492d2e389342b10f41ea52b2f32555a1282b04
Steven Ossorio <usa.s.ossorio@gmail.com>
2023-08-22 18:34:03 -0500

    Updated mongoDB to include billboard. Created billboard page

---
+++ ecommerce-admin/src/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -0,0 +1,27 @@
+import prismadb from "@/lib/prismadb";
+import { BillboardForm } from "./components/billboard-form";
+
+const BillboardFormPage = async ({
+  params,
+}: {
+  params: { billboardId: string };
+}) => {
+  const billboard =
+    params.billboardId === "new"
+      ? null
+      : await prismadb.billboard.findUnique({
+          where: {
+            id: params.billboardId,
+          },
+        });
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardForm initialData={billboard} />
+      </div>
+    </div>
+  );
+};
+
+export default BillboardFormPage;
